95141d63670d1b89ee73e27b46ee0c7aa0b513db
Pietro Scurti <pietroscurti9@gmail.com>
2025-11-28 17:13:38 +0100

    seconda parte completata

--- 02-strings/script.js
+++ 02-strings/script.js
@@ -1,12 +1,15 @@
 // 1. Dichiarazione di variabili
 // Crea una variabile chiamata name e assegnale il tuo nome.
 // Stampa il valore di nome nella console.
-
+const myname = "pietro"
+console.log (myname)
 // 2. Tipi primitivi
 // Crea tre variabili chiamate number, text e bool e assegna rispettivamente
 // un numero, una stringa e un valore booleano.
 // Stampa tutti i valori nella console.
-
+const number = 3
+const text = "testo"
+const bool = true|false; 1|0
 // 3. Sostituisci null nelle righe sottostanti utilizzando operazioni con le stringhe
 // e le variabili sopra in modo da stampare correttamente 
 // in console la stringa con il nome completo
